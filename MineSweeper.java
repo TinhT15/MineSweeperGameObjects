0fe873bd0dad93e947b50e085afdaf08314c3460
Tinh <tinh.tran@kennedyhs.org>
2021-03-15 21:19:44 -0700

    I have added more members and objects.

--- MineSweeper.java
+++ MineSweeper.java
@@ -8,7 +8,20 @@
 public class MineSweeper
 {
     public static void main(String[] args) {
-    MineSweeperGameObjects myGame = new MineSweeperGameObjects();     // Create a new MinesweeperGame object
-    myGame.printGameBoard();      // Call the printGameBoard() method
+    MineSweeperGameObjects theSolutionGrid = new MineSweeperGameObjects();   // Create a new object to hold the solution grid
+    theSolutionGrid.printGrid();      // Call the printGameBoard() method
+    theSolutionGrid.setValueAtPosition(0,0,9); // Set a mine at position 0,0        
+    theSolutionGrid.setValueAtPosition(1,1,9); // Set a mine at position 1,1    
+    theSolutionGrid.setValueAtPosition(2,2,9); // Set a mine at position 2,2   
+    theSolutionGrid.setValueAtPosition(3,3,9); // Set a mine at position 3,3   
+    theSolutionGrid.setValueAtPosition(4,4,9); // Set a mine at position 4,4
+    theSolutionGrid.getNumberOfMines(1,0);   //Get the number of mines around position x,y
+    theSolutionGrid.isGridFilled(); 
+    theSolutionGrid.printGrid();      // See what the grid looks like
+    
+    MineSweeperGameObjects theUserGrid = new MineSweeperGameObjects();   // Create a new object to hold the user grid
+    
+    MineSweeperGameObjects theTestGrid = new MineSweeperGameObjects();   // Create a new object to hold the solution test
+
   }
 }
--- MineSweeperGameObjects.java
+++ MineSweeperGameObjects.java
@@ -14,10 +14,9 @@ public class MineSweeperGameObjects
                          {0,0,0,0,0},
                          {0,0,0,0,0},
                                };
-     int numberOfMines = 0;
      int myArraySize = 5;
      
-     public void printGameBoard() {
+     public void printGrid() {
         System.out.println("Here is the minesweeper grid map.");
         for (int x = 0; x < myArraySize; x++) {
                 for (int y = 0; y < myArraySize; y++) { 
@@ -44,5 +43,79 @@ public class MineSweeperGameObjects
                 System.out.println("");
                 
         }        
-    }        
+    }
+    
+    public int getNumberOfMines(int x, int y){
+        int numberOfMines = 0;
+        for (int i = 0; i < myArraySize; i++) {
+                for (int j = 0; j < myArraySize; j++) { 
+                    
+                    if (j == y-1)
+                    {
+                        if (i == x-1 || i == x || i == x+1)
+                        {
+                            if (myArray[i][j] == 9)
+                            {
+                                numberOfMines++;
+                            }
+                        }
+                    }
+                    else if (j == y)
+                    {
+                        if (i == x-1 || i == x+1)
+                        {
+                            if (myArray[i][j] == 9)
+                            {
+                                numberOfMines++;
+                            }
+                        }
+                    }
+                    else if (j == y+1)
+                    {
+                        if (i == x-1 || i == x || i == x+1)
+                        {
+                            if (myArray[i][j] == 9)
+                            {
+                                numberOfMines++;
+                            }
+                        }
+                    }                   
+                }
+        }
+        return numberOfMines;
+    }    
+    
+    public boolean isGridFilled(){
+          for (int x = 0; x < myArraySize; x++) {
+                for (int y = 0; y < myArraySize; y++) { 
+                    
+                    int myInt = myArray[x][y];
+                    
+                    if (myInt == 0){
+                        //0 means not filled
+                        return false;
+                    }
+                }
+          }
+          return true;
+    }
+    
+    public void setValueAtPosition(int x, int y, int value){
+        myArray[x][y] = value;
+    }
+    
+    public int getValueAtPosition(int x, int y){
+        return myArray[x][y];
+    }
+    
+    public boolean hitAMine(int x, int y){
+        if (myArray[x][y] == 9)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
